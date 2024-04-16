c26b9975d7a9459381b12c6722ea1b8c70230a4a
vaibhav <vaibhav.waghmare@techprescient.com>
2024-04-16 17:58:40 +0530

    Added configuration changes in application properties and added jwt token validation with exception handling for jwt token

--- src/main/java/com/demo/importer/ImporterApplication.java
+++ src/main/java/com/demo/importer/ImporterApplication.java
@@ -1,9 +1,12 @@
 package com.demo.importer;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class ImporterApplication {
 
 	public static void main(String[] args) {
