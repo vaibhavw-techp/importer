0116b8557a66c383f118b3f2322ab2f351f18bdd
vaibhav <vaibhav.waghmare@techprescient.com>
2024-04-16 17:27:08 +0530

    Added student controller for student addition

---
+++ src/main/java/com/demo/importer/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.demo.importer.controller;
+
+import com.demo.importer.dto.StudentAdditionDto;
+import com.demo.importer.dto.StudentDisplayDto;
+import com.demo.importer.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public List<StudentDisplayDto> addStudent(@RequestBody List<StudentAdditionDto> students, @RequestHeader("Authorization") String token) {
+        return studentService.saveDummyStudent2(students,token);
+    }
+
+}
