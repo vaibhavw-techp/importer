a0894c04aefad2f6d66488d9be4918fab9791a22
vaibhav <vaibhav.waghmare@techprescient.com>
2024-04-16 17:41:07 +0530

    Added Log repository

---
+++ src/main/java/com/demo/importer/repository/LogRepository.java
@@ -0,0 +1,11 @@
+package com.demo.importer.repository;
+
+import com.demo.importer.dto.LogAddtionDto;
+import com.demo.importer.entity.LogEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface LogRepository {
+    void saveStudentLog(@Param("log") LogEntity log);
+}
