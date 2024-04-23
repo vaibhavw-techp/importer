cb7607fb86249a43303ebe27457ad06fd318ddcd
vaibhav <vaibhav.waghmare@techprescient.com>
2024-04-23 13:45:42 +0530

    Added initial service changes for Apache Kafka producer application

--- src/main/java/com/demo/importer/controller/StudentController.java
+++ src/main/java/com/demo/importer/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.demo.importer.dto.LogDisplayDto;
 import com.demo.importer.dto.StudentAdditionDto;
 import com.demo.importer.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +23,13 @@ public class StudentController {
 
     @PostMapping
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public List<LogDisplayDto> addStudent(@RequestBody List<StudentAdditionDto> students) {
-        return studentService.saveStudent(students);
+    public ResponseEntity<?> addStudent(@RequestBody List<StudentAdditionDto> students) {
+        try {
+            studentService.saveStudent(students);
+            return ResponseEntity.ok("Successful");
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
--- src/main/java/com/demo/importer/service/StudentService.java
+++ src/main/java/com/demo/importer/service/StudentService.java
@@ -1,77 +1,37 @@
 package com.demo.importer.service;
 
-import com.demo.importer.dto.LogDisplayDto;
+
 import com.demo.importer.dto.StudentAdditionDto;
-import com.demo.importer.dto.StudentDisplayDto;
-import com.demo.importer.dto.LogAddtionDto;
-import com.demo.importer.entity.LogEntity;
 import com.demo.importer.mapstruct.LogMapper;
 import com.demo.importer.repository.LogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class StudentService {
 
-    @Value("${post.student}")
-    private String ADD_STUDENT_URL;
-    @Autowired
-    private LogRepository logRepository;
     @Autowired
     private LogMapper logMapper;
+    @Autowired
+    private KafkaTemplate<String, List<StudentAdditionDto>> kafkaTemplate;
 
-    public List<LogDisplayDto> saveStudent(List<StudentAdditionDto> students) {
-        List<LogDisplayDto> logDisplayDtos = new ArrayList<>();
-        RestTemplate restTemplate = new RestTemplate();
+    public void saveStudent(List<StudentAdditionDto> students) {
+        CompletableFuture<SendResult<String, List<StudentAdditionDto>>> future = kafkaTemplate.send("student-topic", students);
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Jwt jwt = (Jwt) authentication.getPrincipal();
-        String token = jwt.getTokenValue();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + token);
-
-        for (StudentAdditionDto student : students) {
-            try {
-                HttpEntity<StudentAdditionDto> requestEntity = new HttpEntity<>(student, headers);
-                ResponseEntity<StudentDisplayDto> response = restTemplate.postForEntity(ADD_STUDENT_URL, requestEntity, StudentDisplayDto.class);
-                LogEntity logEntity = saveLog(student, HttpStatus.OK.value(), "Successful");
-                logDisplayDtos.add(logMapper.mapLogEntityToLogDisplayDto(logEntity));
-            } catch (HttpClientErrorException | HttpServerErrorException ex) {
-                int statusCode = ex.getStatusCode().value();
-                LogEntity logEntity = saveLog(student, statusCode, HttpStatus.valueOf(statusCode).getReasonPhrase());
-                logDisplayDtos.add(logMapper.mapLogEntityToLogDisplayDto(logEntity));
-            } catch (Exception ex) {
-                int statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
-                String errorMessage = "Internal Server Error: " + ex.getMessage();
-                LogEntity logEntity = saveLog(student, statusCode, errorMessage);
-                logDisplayDtos.add(logMapper.mapLogEntityToLogDisplayDto(logEntity));
+        future.whenComplete((result,ex)->{
+            if(ex == null) {
+                System.out.println("Send message successfull, with current Offset: " + result.getRecordMetadata().offset());
             }
-        }
-
-        return logDisplayDtos;
-    }
-
-    LogEntity saveLog(StudentAdditionDto student, int statusCode, String status) {
-        LogAddtionDto logDto = new LogAddtionDto(student.getName(), student.getEmail(), statusCode, LocalDateTime.now(), status);
-        LogEntity logEntity = logMapper.mapLogAdditionDtoToLogEntity(logDto);
-        logRepository.saveStudentLog(logEntity);
-        return logEntity;
+            else {
+                System.out.println("Unable to send message!!" + ex.getMessage());
+            }
+        });
     }
 
 }
